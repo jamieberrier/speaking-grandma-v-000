5f81336c79872910974666709aa972882195942a
jamieberrier <berrier.jamie@gmail.com>
2018-07-24 17:31:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,14 +14,12 @@
 
 def speak_to_grandma(phrase)
 
-  if phrase != phrase.upcase()
+  if phrase == "I LOVE YOU GRANDMA!"
+      puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.upcase()
     puts "HUH?! SPEAK UP, SONNY!"
   else
     puts "NO, NOT SINCE 1938!"
-  end
-
-  if phrase == "I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMPKIN!"
   end
 
 end
