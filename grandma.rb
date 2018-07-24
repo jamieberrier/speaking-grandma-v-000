0b65d2bfe153b5137941a6e480aa9e011f1b3620
jamieberrier <berrier.jamie@gmail.com>
2018-07-24 17:24:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
+  
     if phrase == "I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase == phrase.uppercase
+    elsif phrase == phrase.upper
       puts "NO, NOT SINCE 1938!"
     else
       puts "HUH?! SPEAK UP, SONNY!"
